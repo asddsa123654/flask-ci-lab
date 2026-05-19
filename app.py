f85c6f9d900ac8928f1bfbd8b9f310574cc4bc8d
asddsa123654 <aholavko90@gmail.com>
2026-05-19 05:32:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 from flask import Flask, jsonify
-import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return jsonify({"message": "Hello, CI/CD!"})
-
-
-@app.route('/health')
-def health():
-    return jsonify({"status": "healthy"})
-
-
-@app.route('/info')
-def info():
-    return jsonify({
-        "version": os.getenv("APP_VERSION", "1.0.0"),
-        "environment": os.getenv("ENVIRONMENT", "development")
-    })
+def home():
+    return jsonify({"status": "success", "message": " Flask API работает!"})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000).0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
